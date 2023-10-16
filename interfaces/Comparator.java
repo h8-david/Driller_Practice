c2117708230a6d57867ba467b632ff77e201f839
h8-david <david.aycock2020@gmail.com>
2023-10-16 15:16:27 -0500

    Comment correction on comparator interface

--- interfaces/Comparator.java
+++ interfaces/Comparator.java
@@ -15,8 +15,8 @@ public interface Comparator<T>
 	 * @param item1 - item to compare
 	 * @param item2 - item to compare against
 	 * @return 0 if the two items are equivalent,
-	 * 		   1 if item1 > item 2
-	 * 		   -1 if item1 < item 2 
+	 * 		   1 or larger if item1 > item 2
+	 * 		   -1 or less if item1 < item 2 
 	 */
 	 int compare(T item1, T item2);
 }
